b56241734ca82b3139473a8cf701f0814ac43970
Gergely Laborci <gergely@laborci.hu>
2019-10-15 19:12:03 +0200

    Service invoke

--- src/System/ServiceManager/Service.php
+++ src/System/ServiceManager/Service.php
@@ -1,10 +1,12 @@
 <?php namespace Eternity2\System\ServiceManager;
 
-trait Service {
-	public static function Service(...$args):self{
-		if(count($args)){
+trait Service{
+	public static function Service(...$args): self{
+		if (count($args)){
 			return ServiceContainer::get(get_called_class())(...$args);
 		}
 		return ServiceContainer::get(get_called_class());
 	}
+
+	public static function Invoke(...$args){ return static::Service()(...$args); }
 }
